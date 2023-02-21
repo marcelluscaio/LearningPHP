91f9618f49900d3d5d18b5006bb55d9f15f0b8f3
Caio Cabral <ocaiocabral@gmail.com>
2023-02-21 17:51:19 -0400

    feat: function

--- ordena.php
+++ ordena.php
@@ -16,6 +16,12 @@ $notas_ordenadas = $notas.sort();
 
 $nota_especifica = $notas.find(7);
 
+function findNota($num){
+   $notas.find($num);
+}
+
+findNota(8);
+
 var_dump($notas);
 var_dump($notas_ordenadas);
 var_dump($nota_especifica);
