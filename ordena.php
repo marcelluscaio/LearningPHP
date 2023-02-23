064831839d2de4aa40d209a5b6fd565d389e9715
Caio Cabral <ocaiocabral@gmail.com>
2023-02-23 19:39:57 -0400

    feat: comparing two arrays

--- ordena.php
+++ ordena.php
@@ -14,6 +14,31 @@ $notas = [
 
 $notas_ordenadas = $notas;
 sort($notas_ordenadas);
+echo 'notas';
+var_dump($notas);
+
+echo 'notas';
+var_dump($notas_ordenadas);
+
+$notas = [ 
+   ['Aluno' => 'Maria', 'nota' => 8], 
+   ['Aluno' => 'Ana', 'nota' => 5]
+];
+
+
+function ordenaNotas(array $nota1, array $nota2) :int{
+   if($nota1['nota'] > $nota2['nota']){
+      return 1;
+   }
+   else if($nota2['nota'] > $nota1['nota']){
+      return -1;
+   }
+   return 0;
+} 
+
+usort(array: $notas, callback: 'ordenaNotas');
+
+var_dump($notas) ;
 
 /* $nota_especifica = $notas.find(7);
 
@@ -22,9 +47,3 @@ function findNota($num){
 }
 
 findNota(8); */
-
-echo 'notas';
-var_dump($notas);
-
-echo 'notas';
-var_dump($notas_ordenadas);
